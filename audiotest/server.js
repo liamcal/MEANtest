04a1882b4092d94c6b4e3918079a63b4de986cb3
Liam Callaway <coolboyrocx@gmail.com>
2017-04-06 12:32:42 +1000

    Added AudioTest demo

---
+++ audiotest/server.js
@@ -0,0 +1,11 @@
+const express = require('express'); //Express JS Webframework
+const app = express(); //Create the backend application
+
+app.use("/", express.static(__dirname));
+app.listen(3000, function() {
+    console.log('listening on port 3000');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
